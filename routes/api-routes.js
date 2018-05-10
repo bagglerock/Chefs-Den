3408413fa9829890332371f93ec9e571e1c2f934
Oscar Villalta <bagglerock@gmail.com>
2018-05-10 14:00:55 -0400

    some css added for fun

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,25 +1,21 @@
 var db = require("../models");
 var request = require("request");
 
+var app_id = "3f2c9a8d";
+var app_key = "e92f49e132a104a2da4588b89f9f4eea";
+
 // Create all our routes and set up logic within those routes where required.
 
 module.exports = function (app) {
 
     app.get("/", function (req, res) {
-        request("http://www.omdbapi.com/?t=" + "The Matrix" + "&y=&plot=short&apikey=trilogy",
+        request("http://api.yummly.com/v1/api/recipes?_app_id=" + app_id + "&_app_key=" + app_key + "&chicken",
             function(error, response, body) {
                 if(!error && response.statusCode === 200){
-                    //console.log(body);
+                    //  have to parse the response to JSON
                     var hbsObject = JSON.parse(body);
-                    /*var hbsObject = {
-                        Title: body.title
-                    };*/
-                    console.log(hbsObject);
                 }
-
-
             res.render("index", hbsObject);
-            //res.json(hbsObject);
         })
     });
 }
